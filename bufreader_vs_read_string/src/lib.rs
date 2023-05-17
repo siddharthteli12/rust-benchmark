ae2842ec8135e08cec0a56101fa16da59d8e205d
siddharthteli12 <telisiddharth123@yahoo.in>
2023-05-17 22:31:22 +0530

    Refactor: (buf reader vs read to string) use lines method rather than read line in buffer.X

--- bufreader_vs_read_string/src/lib.rs
+++ bufreader_vs_read_string/src/lib.rs
@@ -4,25 +4,21 @@ use std::{
 };
 
 // Find pattern in file using buffer read with read line.
-fn buffer(pattern: String, path: String) -> Result<Vec<(usize, String)>, Error> {
-    let mut reader = BufReader::new(File::open(path)?);
-    let mut content = String::new();
+pub fn buffer(pattern: String, path: String) -> Result<Vec<(usize, String)>, Error> {
+    let reader = BufReader::new(File::open(path)?);
+    let content = String::new();
     let mut result_vec: Vec<(usize, String)> = vec![];
-    let mut counter = 0;
-    while reader.read_line(&mut content)? != 0_usize {
-        counter = counter + 1;
-        if content.is_empty() {
-            break;
-        }
+
+    for (counter, line) in reader.lines().enumerate() {
         if content.contains(&pattern) {
-            result_vec.push((counter, content.clone()));
+            result_vec.push((counter, line?.to_string()));
         }
     }
     Ok(result_vec)
 }
 
 // Find pattern in file using read to string at once.
-fn read_to_string(pattern: String, path: String) -> Result<Vec<(usize, String)>, Error> {
+pub fn read_to_string(pattern: String, path: String) -> Result<Vec<(usize, String)>, Error> {
     let content = fs::read_to_string(path)?;
     let mut result_vec: Vec<(usize, String)> = vec![];
 
